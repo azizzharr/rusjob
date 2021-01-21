64cfccf0c625cac68b86f260a070a3f401b3fb77
азиз жаркынбаев <azizzarkynbaev@mail.com>
2021-01-21 22:02:07 +0600

    list

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react"
 import Header from "../components/header/header"
+import Listjob from "../components/listjob/listjob"
+
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                <Listjob/>
+                
             </div>
         )
     }
---
+++ src/components/listjob/listjob.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react"
+import "./listjob.scss"
+
+class App extends Component {
+    render() {
+        return (
+            <div className="block">
+<div className="block_list" >
+
+    <div className="block_innenlist">
+
+    
+    <div >
+    Водители CDL Class A, Local and Long
+    </div>
+    <span className="block_span">
+    Водители CDL 
+    </span>
+    <div>
+ 3 неделю назад
+    </div>
+   
+
+</div>
+</div>
+<br/>
+
+<div className="block_list" >
+
+    <div className="block_innenlist">
+
+    
+    <div >
+    Водители CDL Class A, Local and Long
+    </div>
+    <span className="block_span">
+    Водители CDL 
+    </span>
+    <div>
+ 3 неделю назад
+    </div>
+   
+
+    
+</div>
+
+
+</div>
+
+
+<br/>
+
+<div className="block_list" >
+
+    <div className="block_innenlist">
+
+    
+    <div >
+    Водители CDL Class A, Local and Long
+    </div>
+    <span className="block_span">
+    Водители CDL 
+    </span>
+    <div>
+ 3 неделю назад
+    </div>
+   
+
+    
+</div>
+
+
+</div>
+
+
+<br/>
+
+<div className="block_list" >
+
+    <div className="block_innenlist">
+
+    
+    <div >
+    Водители CDL Class A, Local and Long
+    </div>
+    <span className="block_span">
+    Водители CDL 
+    </span>
+    <div>
+ 3 неделю назад
+    </div>
+   
+
+    
+</div>
+
+
+</div>
+
+<br/>
+<div className="block_list" >
+
+    <div className="block_innenlist">
+
+    
+    <div >
+    Водители CDL Class A, Local and Long
+    </div>
+    <span className="block_span">
+    Водители CDL 
+    </span>
+    <div>
+ 3 неделю назад
+    </div>
+   
+
+    
+</div>
+
+
+</div>  
+
+            </div>
+        )
+    }
+}
+
+export default App;
